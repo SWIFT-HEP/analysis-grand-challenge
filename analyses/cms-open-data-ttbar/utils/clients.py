b4b961f459f5fdac0a5afa512f7e0a42047915ac
Sam Eriksen <sam.eriksen@bristol.ac.uk>
2023-09-11 18:01:59 +0100

    feat: add dask-dirac client option

--- analyses/cms-open-data-ttbar/utils/clients.py
+++ analyses/cms-open-data-ttbar/utils/clients.py
@@ -19,6 +19,27 @@ def get_client(af="coffea_casa"):
         from dask.distributed import Client
 
         client = Client()
+        
+    elif af == "DIRAC":
+        from dask_dirac import DiracCluster
+        from dask.distributed import Client
+        
+        cluster = DiracCluster(cores=8,
+                       memory="24GB",
+                       scheduler_options={"port": 8786},
+                       dirac_site="LCG.UKI-SOUTHGRID-RALPP.uk",
+                       cert_path="/users/ak18773/SWIFT_HEP/dev_dirac/diracos/etc/grid-security/certificates",
+                       owner_group="gridpp_user",
+                       user_proxy="/tmp/x509up_u397871",
+                       submission_url="https://diracdev.grid.hep.ph.ic.ac.uk:8444",
+                    )
+        
+        cluster.scale(jobs=5)
+        
+        print("Please allow up to 60 minutes for the first worker to connect.")
+        print(f"Cluster dashboard: {str(cluster.dashboard_link)}")
+        
+        client = Client(cluster)
 
     else:
         raise NotImplementedError(f"unknown analysis facility: {af}")
